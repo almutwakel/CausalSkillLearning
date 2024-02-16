35b19f22dfcbe7f986aa1969ede39ea782e8f276
tanmayshankar <tanmay.shankar@gmail.com>
2024-02-16 12:20:20 -0500

    Using new data

--- Experiments/RealWorldHumanRigid_DataLoader.py
+++ Experiments/RealWorldHumanRigid_DataLoader.py
@@ -1113,7 +1113,8 @@ class RealWorldHumanRigid_Dataset(Dataset):
 			# "New_Task_Demo_Array{}_HDImages.npy"
 			# self.files.append(np.load("{0}/{1}/New_Task_Demo_Array_HDImages.npy".format(self.dataset_directory, self.task_list[i]), allow_pickle=True))
 			# self.files.append(np.load("{0}/{1}/New_Task_Demo_Array_HDImages_NoTagFusion.npy".format(self.dataset_directory, self.task_list[i]), allow_pickle=True))
-			self.files.append(np.load("{0}/{1}/New_Task_Demo_Array_HDImages_NewFreq_RenableTagFusion.npy".format(self.dataset_directory, self.task_list[i]), allow_pickle=True))
+			# self.files.append(np.load("{0}/{1}/New_Task_Demo_Array_HDImages_NewFreq_RenableTagFusion.npy".format(self.dataset_directory, self.task_list[i]), allow_pickle=True))			
+			self.files.append(np.load("{0}/{1}/New_Task_Demo_Array_HDImages_ReorderedObjects.npy".format(self.dataset_directory, self.task_list[i]), allow_pickle=True))
 			
 
 	def __getitem__(self, index):
