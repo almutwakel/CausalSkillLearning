5df579aed7714f761aaea2d67cd4933808c6a737
tanmayshankar <tanmay.shankar@gmail.com>
2024-02-21 17:56:25 -0500

    debug

--- Experiments/PolicyManagers.py
+++ Experiments/PolicyManagers.py
@@ -2820,6 +2820,9 @@ class PolicyManager_Pretrain(PolicyManager_BaseClass):
 		ax = fig.gca()				
 
 		for k in range(len(embedded_zs)):
+			
+			print("######################")
+			print(k, self.task_id_set[k])
 			# For all embedded z's, add index of this z to the appropriate composiitonal task index list.
 			per_compositional_task_indices[self.dataset.task_list[self.task_id_set[k]]].append(k)
 			
